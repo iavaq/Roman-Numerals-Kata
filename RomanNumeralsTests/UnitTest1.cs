4555d830dc193a4be959057a3d47ba60a8d70369
Ia <iavaquilar@gmail.com>
2022-05-22 21:02:05 +0100

    project setup

---
+++ RomanNumeralsTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RomanNumeralsTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
